d71c9c9adc7c7f934c4a2cc26d995f1bf6dcd862
kinhatula <kinhatula@gmail.com>
2024-07-04 10:04:07 +0700

    sua_file_500

--- src/pages/500 copy.tsx
+++ src/pages/500 copy.tsx
@@ -1,11 +1,5 @@
-import Typography from '@mui/material/Typography'
+console.log('chào bạn')
+console.log('chào bạn')
+console.log('chào bạn')
+console.log('chào bạn')
 
-const Error500 = () => {
-  return (
-    <Typography variant='h2' sx={{ mb: 1.5 }}>
-      Oops, something went wrong!
-    </Typography>
-  )
-}
-
-export default Error500
